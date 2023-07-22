7570759e830269cc04ca6fbf36f75a429eb71a50
kromate <akpananthony33@gmail.com>
2023-07-22 22:33:14 +0100

    -

---
+++ src/components/invoice/Setup/index.vue
@@ -0,0 +1,17 @@
+
+<template>
+	<section class="flex flex-col sub_section">
+		<Stepper />
+		<p class="mt-12">
+			section 1
+		</p>
+	</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,7 @@
 <template>
-	<main class="grid grid-cols-2 gap-4 p-4 ">
-		<section class="flex flex-col">
-			section 1
-		</section>
-		<section class="flex flex-col">
-			section 2
-		</section>
+	<main class="grid grid-cols-2 gap-4 p-4 h-screen">
+		<InvoiceSetup />
+		<InvoiceOutput />
 	</main>
 </template>
 
