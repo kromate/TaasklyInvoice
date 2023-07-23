1ef62a2addcbf31055dfaaaa2c4916e81241ccee
SammieKei40 <sammiekei9@gmail.com>
2023-07-23 21:20:23 +0100

    Retouched some codes

--- src/pages/invoice.vue
+++ src/pages/invoice.vue
@@ -1,25 +1,12 @@
 <template>
-	<div>
-		<invoiceModal v-if="showModal" @close="handleModalClose" />
-
-		<div class="flex justify-center items-center min-h-screen">
-			<button type="pry" class="text-white bg-primary py-4 px-4  rounded border border-gray-200 text-sm font-medium " @click="openModal">
-				Open Modal
-			</button>
-		</div>
-	</div>
+	<main class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 h-screen">
+		<InvoiceSetup />
+		<invoiceModal @close="handleModalClose" />
+	</main>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import VButton from '../components/v-button.vue'
 import invoiceModal from '../components/invoiceModal.vue'
-
-const showModal = ref(false)
-
-function openModal() {
-  showModal.value = true
-}
 
 function handleModalClose() {
   showModal.value = false
