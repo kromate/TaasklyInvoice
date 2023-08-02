31f6b64a72df14ded99f578b050921ebd1c64738
kromate <akpananthony33@gmail.com>
2023-08-03 00:28:04 +0100

    updated the landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -32,8 +32,14 @@
 				</h1>
 				<p class="mt-6 text-lg leading-8 text-grey_two">
 					Say goodbye to the hassle of manual invoicing and hello to streamlined efficiency. Whether you're a freelancer, small business owner, or independent contractor,
-					<a href="https://taaskly.xyz" target="_blank" rel="noopener noreferrer" class="underline text-secondary">Taaskly</a>  Invoice has got you covered.
+					<a href="https://taaskly.xyz" target="_blank" rel="noopener noreferrer" class="underline text-secondary">Taaskly</a> Invoice has got you covered.
 				</p>
+				<div class="flex flex-col gap-3 text-secondary text-lg mt-4">
+					<span v-for="item in benefits" :key="item.name" class="flex items-center gap-3">
+						<CheckCircleIcon class="text-dark w-6" />
+						<span v-html="item.name" />
+					</span>
+				</div>
 				<div class="mt-10 flex items-center gap-x-6">
 					<nuxt-link class="btn-secondary" to="/invoice">
 						Get started
@@ -44,13 +50,7 @@
 			<div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
 				<div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
 					<div class="-m-2 rounded-xl bg-grey_four p-2 ring-1 ring-inset ring-dark lg:-m-4 lg:rounded-2xl lg:p-4">
-						<img
-							src="@/assets/images/cta.svg"
-							alt="App screenshot"
-							width="2432"
-							height="1442"
-							class="w-[40rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
-						>
+						<img src="@/assets/images/cta.svg" alt="App screenshot" width="2432" height="1442" class="w-[40rem] rounded-md shadow-2xl ring-1 ring-gray-900/10">
 					</div>
 				</div>
 			</div>
@@ -58,6 +58,29 @@
 	</div>
 </template>
 
-	<script setup>
-		import { ChevronRightIcon } from '@heroicons/vue/20/solid'
-	</script>
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/20/solid'
+
+const benefits = ref([
+	{
+		name: 'Open Source',
+		description: 'Taaskly Invoice is open source and free to use. You can contribute to the project on Github.',
+		icon: CheckCircleIcon
+	},
+	{
+		name: 'Easy to use',
+		description: 'Taaskly Invoice is easy to use and can be used by anyone. No coding skills required.',
+		icon: CheckCircleIcon
+	},
+	{
+		name: 'Customizable',
+		description: 'Taaskly Invoice is customizable. You can change the colors, logo, and more.',
+		icon: CheckCircleIcon
+	},
+	{
+		name: 'Multiple template <span class="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">Coming Soon</span>',
+		description: 'Taaskly Invoice is customizable. You can change the colors, logo, and more.',
+		icon: CheckCircleIcon
+	}
+])
+</script>
